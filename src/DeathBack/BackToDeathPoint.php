770c0eae95764da7719a2f4ea079d3dedb9b4fdf
evermine <edgarsanou@gmail.com>
2015-02-04 22:33:36 +0100

    Added Comments for the developers
    Comments to understand the code ^-^

--- src/DeathBack/BackToDeathPoint.php
+++ src/DeathBack/BackToDeathPoint.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace DeadBack;
+namespace DeadBack; //Define the namespace
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
@@ -15,24 +15,24 @@ class BackToDeathPoint extends PluginBase implements Listener{
 
     public function onEnable(){
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        $this->getLogger()->info("Plugin started successfully!");
+        $this->getLogger()->info("Plugin started successfully!"); //Message on the console to activate the plugin
     }
 
     public function onDisable(){
-        $this->getLogger()->info("Plugin is Disabled!");
+        $this->getLogger()->info("Plugin is Disabled!"); //Message on the console to disable the plugin
     }
 
     public function onCommand(CommandSender $sender, Command $command, $label, array $args){
         switch($command->getName()){
             case "deadback":
                 if(!$sender instanceof Player){
-                    $sender->sendMessage(TextFormat::RED . "Please run this command in-game.");
+                    $sender->sendMessage(TextFormat::RED . "Please run this command in-game."); //Message to run the command in the console
                 }
                 if(!isset($this->lastdeath[$sender->getName()])){
-                    $sender->sendMessage(TextFormat::RED . "[DeadBack] You can only run this command after you die.");
+                    $sender->sendMessage(TextFormat::RED . "[DeadBack] You can only run this command after you die."); //Message when you haven't a previous position
                 }else{
                     $sender->teleport($this->lastdeath[$sender->getName()][0], $this->lastdeath[$sender->getName()][1], $this->lastdeath[$sender->getName()][2]); // In order: 0 = Position object, 1 = Yaw, 2 = Pitch
-                    $sender->sendMessage("[DeadBack] Teleporting to your death location.");
+                    $sender->sendMessage("[DeadBack] Teleporting to your death location.");//Message when teleport to the last location
                     unset($this->lastdeath[$sender->getName()]);
                 }
                 break;
